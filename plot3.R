9d54a8d5470d310410039e078835d36f1d9a821a
Todd Graves <tgraves@austin.utexas.edu>
2015-07-12 18:24:00 -0500

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -3,7 +3,7 @@ download.file("https://archive.ics.uci.edu/ml/machine-learning-databases/00235/h
 all <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", colClasses = c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"),na.strings = "?")
 inrange <- all[(all[,1] == "1/2/2007") | (all[,1] == "2/2/2007"),]
 
-df<-as.data.frame(data1)
+df<-as.data.frame(inrange)
 df$dateTime <-strptime(paste(df$Date, df$Time, sep=" "), format="%d/%m/%Y %H:%M:%S")
 
 png(filename="plot3.png", width=480, height=480)
